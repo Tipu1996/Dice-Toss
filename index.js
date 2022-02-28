c2292480551e305ee31b353eba2ee8a438bf5a0b
Tipu1996 <67307877+Tipu1996@users.noreply.github.com>
2022-02-28 02:31:19 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -8,3 +8,15 @@ var y;
 y = Math.floor(Math.random() * 6) + 1;
 var source2 = "images/dice" + y + ".png";
 document.querySelectorAll("img")[1].setAttribute("src", source2)
+
+if(x > y){
+  document.querySelector(".refresh").innerHTML = "Player1 Wins!"
+}
+
+else if(y > x){
+  document.querySelector(".refresh").innerHTML = "Player2 Wins!"
+}
+
+else{
+  document.querySelector(".refresh").innerHTML = "It's a DRAW :/"
+}
